0ca3c6b2d8d773e9e79165b71aec0afa0fac884a
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-05-09 16:22:21 +0100

    feat: Using useEffect & useCallback to ensure a HTTP Request is sent onloading and doesn't cause infinite loop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,17 +8,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  let content = <p>Found no movies</p>
-  if (movies.length > 0){
-    content = <p><MoviesList movies={movies}/></p>;
-  }
-  if (error){
-    content = <p>{error}</p>;
-  }  
-  if (isLoading){
-    content = <p>Loading ...</p>;
-  }
-  async function fetchMoviesHandler(){
+  const fetchMoviesHandler = useCallback(async () =>{
     setIsLoading(true);
     setError(null);
 
@@ -42,8 +32,22 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
 
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  let content = <p>Found no movies</p>
+  if (movies.length > 0){
+    content = <p><MoviesList movies={movies}/></p>;
+  }
+  if (error){
+    content = <p>{error}</p>;
+  }  
+  if (isLoading){
+    content = <p>Loading ...</p>;
+  }
 
   return (
     <React.Fragment>
