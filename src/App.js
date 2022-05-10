3151c8a00b5d595dbbc93df5d080f70b47ce15f3
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-05-10 13:21:24 +0100

    feat: Implementing the POST functionality and algo fixing the GET list of movies towork using Firebase

--- src/App.js
+++ src/App.js
@@ -19,16 +19,17 @@ function App() {
         throw new Error('Something went wrong: '+response.status);
       }
       const data = await response.json();
-      
-      const transformedMovies = data.results.map( movieData =>{
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      });
-      setMovies(transformedMovies);
+
+      const loadedMovies = [];
+      for (const key in data){
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        });
+      }
+      setMovies(loadedMovies);
     } catch(error){
       setError(error.message);
     }
@@ -39,8 +40,17 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    // This should be wrapped with Try/Catch
+    const response = await fetch('https://star-wars-react-http-default-rtdb.europe-west1.firebasedatabase.app/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
   }
 
   let content = <p>Found no movies</p>
