48aa096f2b80bee1c49c6203a7f591525484f543
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-05-10 11:40:57 +0100

    feat: Implementing POST data with Firebase API (Non-working version)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect, useCallback} from 'react';
+import AddMovie from './components/AddMovie';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -13,7 +14,7 @@ function App() {
     setError(null);
 
     try{
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://star-wars-react-http-default-rtdb.europe-west1.firebasedatabase.app/movies.json');
       if(!response.ok){
         throw new Error('Something went wrong: '+response.status);
       }
@@ -38,7 +39,12 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
   let content = <p>Found no movies</p>
+
   if (movies.length > 0){
     content = <p><MoviesList movies={movies}/></p>;
   }
@@ -51,6 +57,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>      
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
